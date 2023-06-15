f4f25b54cfa8ed8540e22ee58c10afb649279e6b
clayton <crdcastro5@gmail.com>
2023-06-15 01:01:54 -0700

    better error and input handling for products and releases
    now using pointers to check when fields have been set

--- cmd/api/objects/products.go
+++ cmd/api/objects/products.go
@@ -2,33 +2,40 @@ package objects
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"time"
 )
 
 type Product struct {
-	Id          int32
-	Name        string
-	TarballLink string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Id        int32
+	Name      *string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 func AddProduct(db *sql.DB, new_product Product) error {
-	_, err := db.Exec("INSERT INTO product (id, product_name, product_signed, trivy_signed, trivy_valid, sbom_signed, sbom_valid, last_scanned_at, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-		new_product.Id, new_product.Name, new_product.TarballLink, new_product.CreatedAt.Format("2006-01-02 15:04:05"), new_product.UpdatedAt.Format("2006-01-02 15:04:05"))
-	if err != nil {
-		return err
+	const required_field string = "Missing field \"%s\" required when creating a new product"
+	const sql_error string = "Error creating new product: %w"
+	if new_product.Name == nil {
+		err_msg := fmt.Sprintf(required_field, "Name")
+		return errors.New(err_msg)
+	} else {
+		_, err := db.Exec("INSERT INTO product (name) VALUES (?)", *new_product.Name)
+		if err != nil {
+			return fmt.Errorf(sql_error, err)
+		}
 	}
 	return nil
 }
 
 func GetProduct(db *sql.DB, name string) (Product, error) {
-	var product Product
-	err := db.QueryRow(`SELECT * FROM product WHERE product_name = ?`, name).Scan(&product.Id, &product.Name, &product.CreatedAt, &product.UpdatedAt)
+	var retrieved_product Product
+	err := db.QueryRow(`SELECT * FROM product WHERE name = ?`, name).Scan(&retrieved_product.Id, retrieved_product.Name, &retrieved_product.CreatedAt, &retrieved_product.UpdatedAt)
 	if err != nil {
-		return product, err
+		return retrieved_product, err
 	}
-	return product, nil
+	return retrieved_product, nil
 }
 
 func GetAllProducts(db *sql.DB) ([]Product, error) {
@@ -57,10 +64,9 @@ func GetAllProducts(db *sql.DB) ([]Product, error) {
 	return products, nil
 }
 
-func UpdateProduct(db *sql.DB, updated_product Product) error {
+func UpdateProduct(db *sql.DB, new_name string, name string) error {
 	if _, err := db.Exec(
-		`UPDATE product SET tarball_link = ?, WHERE name = ?;`,
-		updated_product.TarballLink, updated_product.Name); err != nil {
+		`UPDATE product SET name = ?, WHERE name = ?;`, new_name, name); err != nil {
 		return err
 	}
 	return nil
--- cmd/api/objects/releases.go
+++ cmd/api/objects/releases.go
@@ -2,41 +2,74 @@ package objects
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"time"
 )
 
 type Release struct {
 	Id          int32
-	ProductId   int32
-	Name        string
-	TarballLink string
+	ProductId   *int32
+	Name        *string
+	TarballLink *string
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
 
 func AddRelease(db *sql.DB, new_release Release) error {
-	_, err := db.Exec("INSERT INTO releases (product_id, name, tarball_link) VALUES (?, ?, ?)",
-		new_release.ProductId, new_release.Name, new_release.TarballLink)
-	if err != nil {
-		return err
+	const required_field string = "Missing field \"%s\" required when creating a new release"
+	const sql_error string = "Error creating new release: %w"
+	if new_release.ProductId == nil {
+		err_msg := fmt.Sprintf(required_field, "Product Id")
+		return errors.New(err_msg)
+	}
+	if new_release.Name == nil {
+		err_msg := fmt.Sprintf(required_field, "Name")
+		return errors.New(err_msg)
+	}
+	if new_release.TarballLink == nil {
+		_, err := db.Exec(
+			"INSERT INTO releases (product_id, name) VALUES (?, ?)",
+			*new_release.ProductId, *new_release.Name)
+		if err != nil {
+			return fmt.Errorf(sql_error, err)
+		}
+	} else {
+		_, err := db.Exec(
+			"INSERT INTO releases (product_id, name, tarball_link) VALUES (?, ?, ?)",
+			*new_release.ProductId, *new_release.Name, *new_release.TarballLink)
+		if err != nil {
+			return fmt.Errorf(sql_error, err)
+		}
 	}
 	return nil
 }
 
-// TODO: should make sure product id matches too
-func GetRelease(db *sql.DB, name string) (Release, error) {
-	var release Release
-	err := db.QueryRow(`SELECT * FROM releases WHERE name = ?`, name).Scan(&release.Id, &release.ProductId, &release.Name, &release.TarballLink, &release.CreatedAt, &release.UpdatedAt)
-	if err != nil {
-		return release, err
+func GetRelease(db *sql.DB, release Release) (Release, error) {
+	const required_field string = "Missing field \"%s\" required when retrieving a release"
+	const sql_error string = "Error finding release: %w"
+	var retrieved_release Release
+	if release.ProductId == nil {
+		err_msg := fmt.Sprintf(required_field, "Product Id")
+		return retrieved_release, errors.New(err_msg)
 	}
-	return release, nil
+	if release.Name == nil {
+		err_msg := fmt.Sprintf(required_field, "Name")
+		return retrieved_release, errors.New(err_msg)
+	}
+	err := db.QueryRow(
+		`SELECT * FROM releases WHERE name = ? AND product_id = ?`, *release.Name, *release.ProductId).Scan(
+		&retrieved_release.Id, retrieved_release.ProductId, retrieved_release.Name, retrieved_release.TarballLink, &retrieved_release.CreatedAt, &retrieved_release.UpdatedAt)
+	if err != nil {
+		return retrieved_release, fmt.Errorf(sql_error, err)
+	}
+	return retrieved_release, nil
 }
 
 func GetAllReleasesforProduct(db *sql.DB, product_name string) ([]Release, error) {
 
 	product, err := GetProduct(db, product_name)
-	product_id := product.Name
+	product_id := product.Id
 
 	var releases []Release
 	rows, err := db.Query(`SELECT * FROM releases WHERE product_id = ?`, product_id)
@@ -48,7 +81,7 @@ func GetAllReleasesforProduct(db *sql.DB, product_name string) ([]Release, error
 
 	for rows.Next() {
 		var release Release
-		err = rows.Scan(&release.Id, &release.ProductId, &release.Name, &release.TarballLink, &release.CreatedAt, &release.UpdatedAt)
+		err = rows.Scan(&release.Id, release.ProductId, release.Name, release.TarballLink, &release.CreatedAt, &release.UpdatedAt)
 		if err != nil {
 			releases = nil
 			return releases, err
@@ -89,19 +122,44 @@ func GetAllReleases(db *sql.DB) ([]Release, error) {
 	return releases, nil
 }
 
-// TODO: should make sure product id matches too
 func UpdateRelease(db *sql.DB, updated_release Release) error {
-	if _, err := db.Exec(
-		`UPDATE releases SET tarball_link = ?, WHERE name = ?;`,
-		updated_release.TarballLink, updated_release.Name); err != nil {
-		return err
+	const missing_field string = "Missing field %s (needed to locate release in DB)"
+	const sql_error string = "Error updating new release: %w"
+	if updated_release.ProductId == nil {
+		err_msg := fmt.Sprintf(missing_field, "Product Id")
+		return errors.New(err_msg)
+	}
+	if updated_release.Name == nil {
+		err_msg := fmt.Sprintf(missing_field, "Name")
+		return errors.New(err_msg)
+	}
+	if updated_release.TarballLink == nil {
+		return errors.New("No new data to update release with")
+	} else {
+		_, err := db.Exec(
+			`UPDATE releases SET tarball_link = ? WHERE name = ? AND product_id = ?`,
+			*updated_release.TarballLink, *updated_release.Name, *updated_release.ProductId)
+		if err != nil {
+			return fmt.Errorf(sql_error, err)
+		}
 	}
 	return nil
 }
 
-// TODO: should make sure product id matches too
-func DeleteRelease(db *sql.DB, name string) error {
-	_, err := db.Exec(`DELETE FROM releases WHERE name = ?`, name)
+func DeleteRelease(db *sql.DB, release_to_delete Release) error {
+	const missing_field string = "Missing field %s (needed to locate release in DB)"
+	const sql_error string = "Error updating new release: %w"
+	if release_to_delete.ProductId == nil {
+		err_msg := fmt.Sprintf(missing_field, "Product Id")
+		return errors.New(err_msg)
+	}
+	if release_to_delete.Name == nil {
+		err_msg := fmt.Sprintf(missing_field, "Name")
+		return errors.New(err_msg)
+	}
+	_, err := db.Exec(
+		`DELETE FROM releases WHERE name = ? AND product_id = ?`,
+		*release_to_delete.Name, *release_to_delete.ProductId)
 	if err != nil {
 		return err
 	}
