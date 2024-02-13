f9362905a9dd796769b637a866ff4d55830e1030
clayton <crdcastro5@gmail.com>
2024-02-12 23:14:08 -0800

    update: replace alice pkg with ChainHandlers func

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,34 +4,32 @@ import (
 	"carbide-images-api/pkg/api/middleware"
 	"database/sql"
 	"net/http"
-
-	"github.com/justinas/alice"
 )
 
 func NewRouter(db *sql.DB) *http.ServeMux {
 	mux := http.NewServeMux()
-	globalMiddleware := alice.New(middleware.CORS)
-	authMiddleware := alice.New(middleware.CORS, middleware.JWTAuth)
-	mux.Handle("POST /user", globalMiddleware.Then(createUserHandler(db)))
-	mux.Handle("DELETE /user", globalMiddleware.Then(deleteUserHandler(db)))
-	mux.Handle("POST /login", globalMiddleware.Then(loginHandler(db)))
-	mux.Handle("GET /product", authMiddleware.Then(getAllProductsHandler(db)))
-	mux.Handle("POST /product", authMiddleware.Then(createProductHandler(db)))
-	mux.Handle("GET /product/{productName}", authMiddleware.Then(getProductHandler(db)))
-	mux.Handle("PUT /product/{productName}", authMiddleware.Then(updateProductHandler(db)))
-	mux.Handle("DELETE /product/{productName}", authMiddleware.Then(deleteProductHandler(db)))
-	mux.Handle("GET /product/{productName}/release", authMiddleware.Then(getAllReleasesHandler(db)))
-	mux.Handle("POST /product/{productName}/release", authMiddleware.Then(createReleaseHandler(db)))
-	mux.Handle("GET /product/{productName}/release/{releaseName}", authMiddleware.Then(getReleaseHandler(db)))
-	mux.Handle("PUT /product/{productName}/release/{releaseName}", authMiddleware.Then(updateReleaseHandler(db)))
-	mux.Handle("DELETE /product/{proudctName}/release/{releaseName}", authMiddleware.Then(deleteReleaseHandler(db)))
-	mux.Handle("GET /image", authMiddleware.Then(getAllImagesHandler(db)))
-	mux.Handle("POST /image", authMiddleware.Then(createImageHandler(db)))
-	mux.Handle("GET /image/{imageID}", authMiddleware.Then(getImageHandler(db)))
-	mux.Handle("PUT /image/{imageID}", authMiddleware.Then(updateImageHandler(db)))
-	mux.Handle("DELETE /image/{imageID}", authMiddleware.Then(deleteImageHandler(db)))
-	mux.Handle("GET /releaseImageMapping", authMiddleware.Then(getAllReleaseImageMappingsHandler(db)))
-	mux.Handle("POST /releaseImageMapping", authMiddleware.Then(createReleaseImageMappingHandler(db)))
-	mux.Handle("DELETE /releaseImageMapping", authMiddleware.Then(deleteReleaseImageMappingHandler(db)))
+	global := middleware.CORS
+	withAuth := middleware.ChainHandlers(global, middleware.JWTAuth)
+	mux.Handle("POST /user", global(createUserHandler(db)))
+	mux.Handle("DELETE /user", global(deleteUserHandler(db)))
+	mux.Handle("POST /login", global(loginHandler(db)))
+	mux.Handle("GET /product", withAuth(getAllProductsHandler(db)))
+	mux.Handle("POST /product", withAuth(createProductHandler(db)))
+	mux.Handle("GET /product/{productName}", withAuth(getProductHandler(db)))
+	mux.Handle("PUT /product/{productName}", withAuth(updateProductHandler(db)))
+	mux.Handle("DELETE /product/{productName}", withAuth(deleteProductHandler(db)))
+	mux.Handle("GET /product/{productName}/release", withAuth(getAllReleasesHandler(db)))
+	mux.Handle("POST /product/{productName}/release", withAuth(createReleaseHandler(db)))
+	mux.Handle("GET /product/{productName}/release/{releaseName}", withAuth(getReleaseHandler(db)))
+	mux.Handle("PUT /product/{productName}/release/{releaseName}", withAuth(updateReleaseHandler(db)))
+	mux.Handle("DELETE /product/{proudctName}/release/{releaseName}", withAuth(deleteReleaseHandler(db)))
+	mux.Handle("GET /image", withAuth(getAllImagesHandler(db)))
+	mux.Handle("POST /image", withAuth(createImageHandler(db)))
+	mux.Handle("GET /image/{imageID}", withAuth(getImageHandler(db)))
+	mux.Handle("PUT /image/{imageID}", withAuth(updateImageHandler(db)))
+	mux.Handle("DELETE /image/{imageID}", withAuth(deleteImageHandler(db)))
+	mux.Handle("GET /releaseImageMapping", withAuth(getAllReleaseImageMappingsHandler(db)))
+	mux.Handle("POST /releaseImageMapping", withAuth(createReleaseImageMappingHandler(db)))
+	mux.Handle("DELETE /releaseImageMapping", withAuth(deleteReleaseImageMappingHandler(db)))
 	return mux
 }
--- pkg/api/middleware/middleware.go
+++ pkg/api/middleware/middleware.go
@@ -4,6 +4,15 @@ import (
 	"net/http"
 )
 
+func ChainHandlers(handlers ...func(http.Handler) http.Handler) func(http.Handler) http.Handler {
+    return func(finalHandler http.Handler) http.Handler {
+		for _, handler := range handlers {
+            finalHandler = handler(finalHandler)
+        }
+        return finalHandler
+    }
+}
+
 func CORS(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		enableCors(w, r)
